6a25fb5c06dee9111ba3a12df26073c879d13cec
Ximena-Espinoza <167112534+Ximena-Espinoza@users.noreply.github.com>
2024-11-13 20:26:00 -0300

    mainwindow.cpp 3
    agregar cambio de color y arregos de errores en las barras

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,7 +7,7 @@ MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWin
     ui->setupUi(this);
 
     // Texto
-    tituloG = new QLabel("DGraph", this);
+    tituloG = new QLabel("DeGraph", this);
     tituloG->setGeometry(700, 30, 250, 20);   // Posición (x, y) y tamaño ancho x alto
     tituloG->setStyleSheet("font-size: 18px; font-weight: bold; color: #333;");  // Caracteristicas generales
     tituloG->setAlignment(Qt::AlignCenter);   // Centtra el texto
@@ -32,18 +32,38 @@ MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWin
     tituloC->setGeometry(730, 405, 250, 20); // Posición (x, y) y tamaño ancho x alto
     tituloC->setStyleSheet("font-size: 12px; font-weight: bold; color: #333;"); // Caracteristicas generales
 
+    // Funcion para barra de ecuaciones
     Autocompletar();
 
     // Intput minimo
     BarraMin = new QLineEdit(this);             // Crea la barra de texto
+    //Caracterristicas de la barra
+    BarraMin->setStyleSheet("QLineEdit {"
+                            "border-radius: 10px;"
+                            "padding: 5px;"
+                            "background-color: #dcfaf0;"
+                            "}");
     BarraMin->setGeometry(700, 200, 100, 30);   // Los dos primeros lo ubican en el plano, el tercero es el largo y el ultimo el ancho de la barra
+
 
     // Imput maximo
     BarraMax = new QLineEdit(this);              // Crea la barra de texto
+    //Caracterristicas de la barra
+    BarraMax->setStyleSheet("QLineEdit {"
+                            "border-radius: 10px;"
+                            "padding: 5px;"
+                            "background-color: #dcfaf0;"
+                            "}");
     BarraMax->setGeometry(850, 200, 100, 30);    // Los dos primeros lo ubican en el plano, el tercero es el largo y el ultimo el ancho de la barra
 
     // Imput subdivision
     BarraS = new QLineEdit(this);              // Crea la barra de texto
+    //Caracterristicas de la barra
+    BarraS->setStyleSheet("QLineEdit {"
+                            "border-radius: 10px;"
+                            "padding: 5px;"
+                            "background-color: #dcfaf0;"
+                            "}");
     BarraS->setGeometry(700, 300, 250, 30);    // Los dos primeros lo ubican en el plano, el tercero es el largo y el ultimo el ancho de la barra
 
     // Boton check
@@ -96,28 +116,45 @@ void MainWindow::Autocompletar()
 
     // Intput ecuaciones
     BarraE = new QLineEdit(this);             // Crea la barra de texto
+    //Caracterristicas de la barra
+    BarraE->setStyleSheet("QLineEdit {"
+                            "border-radius: 10px;"
+                            "padding: 5px;"
+                            "background-color: #dcfaf0;"
+                            "}");
     BarraE->setGeometry(700, 100, 250, 30);   // Los dos primeros lo ubican en el plano, el tercero es el largo y el ultimo el ancho de la barra
     BarraE->setCompleter(completar);
 }
 
-void MainWindow::GuardaEcu(){
+void MainWindow::GuardaEcu() {
     QString Ecua = BarraE->text();
 
-    QRegularExpression regex("^[0-9\\s\\+\\^\\-\\*/\\(\\)]+$"); // Esta expresión permite números y operadores
+    QRegularExpression regex("^\\s*([\\dx+yz\\-*/^()\\s]+|(\\b(?:sin|cos|tan|asin|acos|atan|atan2|sinh|cosh|tanh|asinh|acosh|atanh|exp|log|log10|log2|pow|sqrt|cbrt|ceil|floor|trunc|round|nearbyint|remainder|fmod|fmax|fmin|fabs|hypot|frexp|ldexp|modf|copysign)\\b\\s*\\(\\s*[\\dx+yz\\-*/^()\\s]+\\)))+$"); // Incluye x, y, z y al menos un número
     QRegularExpressionMatch match = regex.match(Ecua);
 
-    if (match.hasMatch()) { //match.hasMatch() verificar que una cadena contiene solo números y operadores designados
-        // Si la ecuacion es valida
-        GuardarE = Ecua;          // Guardar la ecuacion en la variable creada en .h
+    // Verificar que contenga al menos un número o variable
+    bool contieneNumero = Ecua.contains(QRegularExpression("\\d"));
+
+    if (match.hasMatch() && contieneNumero) { // Verifica que contenga solo caracteres válidos y al menos un número
+        // Si la ecuación es válida
+        BarraE->setStyleSheet("QLineEdit {"
+                              "border-radius: 10px;"
+                              "padding: 5px;"
+                              "background-color: #dcfaf0;"
+                              "}");
+        GuardarE = Ecua;  // Guardar la ecuación en la variable creada en .h
     } else {
-        // Si la ecuacion no es valida
-        if (Ecua== ""){
-            // Error por no ingresar nada
-            QMessageBox::warning(this, "Error", "No ingreso la ecuacion.");
-        }else{
-            // Error por no poner los signos correspondientes
-            QMessageBox::warning(this, "Error", "La ecuación ingresada no es válida. Por favor, usa solo números y operadores básicos (+, -, ^, *, /, ()).");
+        // La ecuación no es válida
+        if (Ecua.isEmpty()) {
+            QMessageBox::warning(this, "Error", "No ingresó la ecuación.");
+        } else {
+            QMessageBox::warning(this, "Error", "La ecuación ingresada no es válida. Por favor, usa solo números, operadores básicos, variables (x, y, z), y funciones válidas como sin(), cos(), etc(Las puedes encontrar en el glosario).");
         }
+        BarraE->setStyleSheet("QLineEdit {"
+                              "border-radius: 10px;"
+                              "padding: 5px;"
+                              "background-color: #f5c2bd;"
+                              "}");
     }
 }
 
@@ -135,8 +172,18 @@ QString MainWindow::Confirmarm(){
         } else {
             QMessageBox::warning(this, "Error", "El valor mínimo no es válido. Por favor, usa solo números.");
         }
+        BarraMin->setStyleSheet("QLineEdit {"
+                                "border-radius: 10px;"
+                                "padding: 5px;"
+                                "background-color: #f5c2bd;"
+                                "}");
         return "";  // Retorna cadena vacía para indicar error
     }
+    BarraMin->setStyleSheet("QLineEdit {"
+                            "border-radius: 10px;"
+                            "padding: 5px;"
+                            "background-color: #dcfaf0;"
+                            "}");
     return m;  // Devuelve el valor si es válido
 }
 
@@ -152,8 +199,18 @@ QString MainWindow::ConfirmarM(){
         } else {
             QMessageBox::warning(this, "Error", "El valor máximo no es válido. Por favor, usa solo números.");
         }
+        BarraMax->setStyleSheet("QLineEdit {"
+                                "border-radius: 10px;"
+                                "padding: 5px;"
+                                "background-color: #f5c2bd;"
+                                "}");
         return "";  // Retorna cadena vacía para indicar error
     }
+    BarraMax->setStyleSheet("QLineEdit {"
+                            "border-radius: 10px;"
+                            "padding: 5px;"
+                            "background-color: #dcfaf0;"
+                            "}");
     return M;  // Devuelve el valor si es válido
 }
 
@@ -164,6 +221,7 @@ void MainWindow::GuardarMm(){
     if (m.isEmpty() || M.isEmpty()) {
         return;  // Termina la función si hubo error previo en m o M
     }
+
     // Conversión segura, dado que m y M son válidos
     bool conversionOk1, conversionOk2;
     double minValue = m.toDouble(&conversionOk1);
@@ -173,11 +231,32 @@ void MainWindow::GuardarMm(){
         if (minValue < maxValue) {
             Guardarm = m;   // Guardar mínimo
             GuardarM = M;   // Guardar máximo
+            BarraMin->setStyleSheet("QLineEdit {"
+                                    "border-radius: 10px;"
+                                    "padding: 5px;"
+                                    "background-color: #dcfaf0;"
+                                    "}");
+            BarraMax->setStyleSheet("QLineEdit {"
+                                    "border-radius: 10px;"
+                                    "padding: 5px;"
+                                    "background-color: #dcfaf0;"
+                                    "}");
         } else {
             QMessageBox::warning(this, "Error", "El mínimo no puede ser mayor o igual que el máximo.");
+            BarraMin->setStyleSheet("QLineEdit {"
+                                    "border-radius: 10px;"
+                                    "padding: 5px;"
+                                    "background-color: #f5c2bd;"
+                                    "}");
+            BarraMax->setStyleSheet("QLineEdit {"
+                                    "border-radius: 10px;"
+                                    "padding: 5px;"
+                                    "background-color: #f5c2bd;"
+                                    "}");
         }
     }
 }
+
 
 // Validacion y guardado de la variable de la subdivision
 void MainWindow::GuardarSub(){
@@ -189,6 +268,11 @@ void MainWindow::GuardarSub(){
     if (match.hasMatch()) { // Verifica que la cadena contiene solo números enteros positivos
         // Si el número es válido y mayor que 0
         GuardarS = Sub; // Guarda la subdivisión en la variable de .h
+        BarraS->setStyleSheet("QLineEdit {"
+                                "border-radius: 10px;"
+                                "padding: 5px;"
+                                "background-color: #dcfaf0;"
+                                "}");
     } else {
         // Si el valor no es válido
         if (Sub == ""){
@@ -198,6 +282,11 @@ void MainWindow::GuardarSub(){
             // Error por ingresar un valor no permitido
             QMessageBox::warning(this, "Error", "La subdivisión debe ser un número entero mayor que 0.");
         }
+        BarraS->setStyleSheet("QLineEdit {"
+                              "border-radius: 10px;"
+                              "padding: 5px;"
+                              "background-color: #f5c2bd;"
+                              "}");
     }
 }
 
@@ -212,25 +301,24 @@ void MainWindow::Check(int state){
 }
 
 
-
 // Funcion SLOT boton ejecutar
 void MainWindow::Ejecutar() {
     try{
-            // Validar y guardar la ecuación ingresada
-            qDebug() << "Ejecutar() - Guardando ecuación...";
-            GuardaEcu();
+        // Validar y guardar la ecuación ingresada
+        qDebug() << "Ejecutar() - Guardando ecuación...";
+        GuardaEcu();
 
-            // Validar y guardar el mínimo y el máximo
-            qDebug() << "Ejecutar() - Guardando mínimo y máximo...";
-            GuardarMm();
+        // Validar y guardar el mínimo y el máximo
+        qDebug() << "Ejecutar() - Guardando mínimo y máximo...";
+        GuardarMm();
 
-            // Validar y guardar la subdivisión
-            qDebug() << "Ejecutar() - Guardando subdivisión...";
-            GuardarSub();
+        // Validar y guardar la subdivisión
+        qDebug() << "Ejecutar() - Guardando subdivisión...";
+        GuardarSub();
 
-            qDebug() << "Ejecutar() - Fin de la función Ejecutar";
-        } catch (const std::exception &e) {
-            QMessageBox::critical(this, "Error crítico", QString("Ocurrió un error inesperado: %1").arg(e.what()));
+        qDebug() << "Ejecutar() - Fin de la función Ejecutar";
+    } catch (const std::exception &e) {
+        QMessageBox::critical(this, "Error crítico", QString("Ocurrió un error inesperado: %1").arg(e.what()));
     }
 }
 
@@ -239,9 +327,29 @@ void MainWindow::Ejecutar() {
 void MainWindow::ReiniciarTodo() {
     // Restablecer las variables
     GuardarE.clear();
+    BarraE->setStyleSheet("QLineEdit {"
+                          "border-radius: 10px;"
+                          "padding: 5px;"
+                          "background-color: #dcfaf0;"
+                          "}");
     GuardarS.clear();
+    BarraS->setStyleSheet("QLineEdit {"
+                          "border-radius: 10px;"
+                          "padding: 5px;"
+                          "background-color: #dcfaf0;"
+                          "}");
     Guardarm.clear();
+    BarraMin->setStyleSheet("QLineEdit {"
+                          "border-radius: 10px;"
+                          "padding: 5px;"
+                          "background-color: #dcfaf0;"
+                          "}");
     GuardarM.clear();
+    BarraMax->setStyleSheet("QLineEdit {"
+                          "border-radius: 10px;"
+                          "padding: 5px;"
+                          "background-color: #dcfaf0;"
+                          "}");
 }
 
 
