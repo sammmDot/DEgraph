ea83222f0af90568e94c307168fadb3d00405836
Ximena-Espinoza <167112534+Ximena-Espinoza@users.noreply.github.com>
2024-11-18 17:00:15 -0300

    main.cpp
    cambio de color del fondo

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.setWindowTitle("DeGraph"); // Título de la ventana
     w.resize(1000, 2000); // Ancho y largo de la ventana
+    w.setStyleSheet("background-color: #e4e4e4;"); // Cambiar el color
     w.show();
     return a.exec();
 }
