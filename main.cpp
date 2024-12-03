aaff81c3078bd75967f67ed00f8172f075c6dfb1
Ximena-Espinoza <167112534+Ximena-Espinoza@users.noreply.github.com>
2024-12-03 17:16:53 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,13 @@
 #include "mainwindow.h"
-
 #include <QApplication>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     MainWindow w;
     w.setWindowTitle("DeGraph"); // Título de la ventana
-    w.resize(1000, 2000); // Ancho y largo de la ventana
+    w.resize(2000, 1000); // Ancho y largo de la ventana
     w.setStyleSheet("background-color: #e4e4e4;"); // Cambiar el color
     w.show();
     return a.exec();
 }
+
