c1d53f2c1371c28f6950a33c193dcd55aa6b5999
Ximena-Espinoza <167112534+Ximena-Espinoza@users.noreply.github.com>
2024-12-03 13:32:43 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,14 +4,13 @@
 
 //para ejecutar el programa desd la terminal:  make && ./interfaz0
 
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.setWindowTitle("DeGraph"); // Título de la ventana
-    w.resize(1000, 1000); // Ancho y largo de la ventana
     w.setStyleSheet("background-color: #e4e4e4;"); // Cambiar el color
-    w.show();
+    w.showFullScreen();
     return a.exec();
 }
-
